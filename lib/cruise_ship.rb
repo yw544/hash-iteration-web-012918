432fc1c5d11cad1b5f7b794df00b9ab1f775baab
York Wang <yw544@nyu.edu>
2018-01-11 09:19:26 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,10 +8,10 @@
 
 def select_winner(passengers)
   # add the code snippet here!
-  winner = []
+  winner = "temp"
   passengers.each do |suite, name|
     if suite == :suite_a && name.start_with?("A")
-      winner<<name
+      winner = name
 end
 end
 winner
